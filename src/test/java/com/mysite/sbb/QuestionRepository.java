eeea79f96800c564c00a897af8b6d362685f28a8
tobbot16 <tobbot16@naver.com>
2025-08-27 15:18:02 +0900

    test8

--- src/test/java/com/mysite/sbb/QuestionRepository.java
+++ src/test/java/com/mysite/sbb/QuestionRepository.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,7 +20,8 @@ class QuestionRepositoryTest {
 	@Autowired
 	private QuestionRepository questionRepository;
 
-
+	@Autowired
+	private AnswerRepository answerRepository;
 
 	@Test
 	void t1() {
@@ -87,6 +89,17 @@ class QuestionRepositoryTest {
 		this.questionRepository.delete(q);
 
 		assertEquals(1, this.questionRepository.count());
+	}
+
+	@Test
+	void t8(){
+		Question question = this.questionRepository.findById(2).get();
+
+		Answer a = new Answer();
+		a.setContent("네 자동으로 생성됩니다.");
+		a.setQuestion(question);//어떤 질문의 답변인지 알기 위해서 question 객체 필요
+		a.setCreateDate(LocalDateTime.now());
+		this.answerRepository.save(a);
 	}
 
 }
