afa9b10e4a998bec852b1c14008283743e3182f7
tobbot16 <tobbot16@naver.com>
2025-08-27 10:47:16 +0900

    controller

--- src/main/java/com/mysite/sbb/SbbApplication.java
+++ src/main/java/com/mysite/sbb/SbbApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SbbApplication {
 
+	MainController mainController = new MainController();
+
 	public static void main(String[] args) {
 		SpringApplication.run(SbbApplication.class, args);
 	}
