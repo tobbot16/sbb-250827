8935e7b48dd0438eaac0230aa7e48af30268bcf8
tobbot16 <tobbot16@naver.com>
2025-08-27 11:21:41 +0900

    yml file git ignore

--- src/main/java/com/mysite/sbb/SbbApplication.java
+++ src/main/java/com/mysite/sbb/SbbApplication.java
@@ -2,8 +2,10 @@ package com.mysite.sbb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootApplication
+@ActiveProfiles("dev")
 public class SbbApplication {
 
 	MainController mainController = new MainController();
