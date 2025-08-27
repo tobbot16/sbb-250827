8e10c66316c517a1bd6a6738a15c5f0b54579800
tobbot16 <tobbot16@naver.com>
2025-08-27 11:09:14 +0900

    add ResponseBody

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,18 +2,21 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public void index(){
-        System.out.println("index() 메서드 호출됨!");
+    @ResponseBody
+    public String index(){
+        return "sbb";
     }
 
     @GetMapping("/hello")
-    public void hello(){
-        System.out.println("hello() 메서드 호출됨!");
+    @ResponseBody
+    public String hello(){
+        return "hello";
     }
 
 
